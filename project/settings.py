e4b414224717fb251835e5ceb3fe9725d939c6ac
wanixarif <wanixarif@gmail.com>
2020-03-30 06:05:16 +0530

    Integrated email based password reset and some UI improvements

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,11 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = "index"
 LOGIN_URL = "login"
+
+#RESETTING PASSWORDS
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("USER_EMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
--- userlogin/forms.py
+++ userlogin/forms.py
@@ -17,7 +17,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["username", "email", ]
 
 
 class ProfileUpdateForm(forms.ModelForm):
